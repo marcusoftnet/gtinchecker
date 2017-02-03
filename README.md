4e902e9630ffc2b38694b88e668b556c79e5dd3d
Marcus Hammarberg <marcus.hammarberg@ica.se>
2017-02-03 14:39:53 +0100

    Final (!) version

--- README.md
+++ README.md
@@ -3,7 +3,10 @@ Looks up GTIN numbers
 
 ## Installation
 
-* Install [ChromeDriver](http://chromedriver.storage.googleapis.com/index.html)
+* Clone repo
 * `npm install`
-* Configure the `gtins` array in the `config.json`
+* Configure the `gtins` array in the `config.json`, for all the GTINS you want to look up
 * `npm start`
+
+## Note
+There's a 1 second timeout so that we don't DDOS the lookup
--- src/index.js
+++ src/index.js
@@ -8,50 +8,41 @@ let scraper = require("./scrapeSplitter.js");
 let fileHelper = require("./fileHelper.js");
 
 let gtins = require('../config/config.json').gtins;
-/*gtins = ['7310155802002']; // ok
-gtins = ["0000073100126",
-"0000073100126",
-"0000073102144",
-"0000073105824",
-"0000073107026",
-"0000073109228",
-"0000073114178",
-"0000073116998",
-"0000073117001",
-"0000073117018",
-"0000073117216",
-"0000073117223"];*/
+//gtins = ['7310155802002']; // ok
 //var gtins = ['7332513106065']; // not found
 //var gtins = ['7393155001149']; // fail
 
 const url = 'http://gepir.gs1.se/web/sv/Home/SearchNumber?KeyCode=GTIN&Method=Search+Party+By+GTIN&LanguageCode=SV&Re=&_=-1&KeyValue=';
-
-let options = {
-	url : url,
-	proxy : 'http://proxy01.ica.se:8080'
-};
  
 fileHelper.recreateFile();
 fileHelper.createHeaderRow();
 
-let rowNo;
-for (var i = 0; i < gtins.length; i++) {
-	options.url = options.url + gtins[i];
-	rowNo = i+1;
+gtins.forEach((gtin, index) => {
+	setTimeout(function () {
+  		handleRequest(gtin, index);
+	}, 1000 * index);		
+});
+
+function handleRequest(gtin, index) {
+	let rowNo = index +1;
+	let options = {
+		url : url + gtin,
+		proxy : 'http://proxy01.ica.se:8080'
+	};
 
 	request(options, function (error, response, body) {
-		console.log("Processing: " + gtins[i]);
-		if (!error && response.statusCode == 200) {
-    		console.log(body) // Show the HTML for the Google homepage. 
-  		}
-	
+		if (error && response.statusCode != 200) {
+    		console.log("Error getting URL: " + options.url);
+    		console.log("Repsonse code: " + response.statusCode);
+    		return;
+  		}	
 		let basicInfo = scraper.scrapeBasicInformation(body);
 		let row;
 
-		if(typeof basicInfo === 'object') {
+		if(typeof basicInfo === 'object') {			
 			row = {
 				RowNo : rowNo,
-				GTIN : gtins[i], 
+				GTIN : gtin, 
 				Företagsnamn : basicInfo.Företagsnamn,
 				GLN : basicInfo.HuvudGLN,
 				GCP : basicInfo.GS1företagsprefix
@@ -60,12 +51,12 @@ for (var i = 0; i < gtins.length; i++) {
 		else{
 			row = {
 				RowNo : rowNo,
-				GTIN : gtins[i], 
+				GTIN : gtin, 
 				Företagsnamn: basicInfo
 			}
 		}
 
 		let rowWritten = fileHelper.appendRow(row);
-		console.log("Wrote row: " + rowNo);
-	})	
+		console.log(`Row: ${rowNo} - ${gtin}`);
+	});
 }
